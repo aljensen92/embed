0481ac34b7b8170a54546d2fb172b4c054af6c3b
aljensen92 <aljensen92@gmail.com>
2023-06-21 08:51:36 -0500

    Update Inboundiframe.js

--- Inboundiframe.js
+++ Inboundiframe.js
@@ -1,9 +1,9 @@
 function embedVenueUrl(url) {
     window.onload = function() {
-        var iframe = document.getElementsByClassName('styled-iframe')[0]; // Get the first iframe with the class 'styled-iframe'
+        var iframe = document.getElementById('venue-iframe'); // Get the existing iframe by its ID
         if (!iframe) {
             iframe = document.createElement('iframe');
-            iframe.className = 'styled-iframe'; // Assign the class to the iframe
+            iframe.id = 'venue-iframe'; // Assign an ID to the iframe
             document.body.appendChild(iframe); // Append the iframe to the body
         }
 
@@ -11,13 +11,14 @@ function embedVenueUrl(url) {
         iframe.setAttribute('width', '100%');
         iframe.setAttribute('height', '100%');
         iframe.setAttribute('frameborder', '0');
+        iframe.className = 'styled-iframe';
 
         var script = document.createElement('script');
         script.src = 'https://cdn.jsdelivr.net/npm/iframe-resizer@4.3.2/js/iframeResizer.min.js';
         document.body.appendChild(script);
 
         script.onload = function() {
-            iFrameResize({ log: true, autoResize: false }, '.styled-iframe'); // Use the class of the iframe and set autoResize to false
+            iFrameResize({ log: true, autoResize: false }, '#venue-iframe'); // Use the ID of the iframe and set autoResize to false
         };
     };
 };
